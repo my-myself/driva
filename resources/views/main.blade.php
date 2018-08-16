0da6fcc07d030edb9b1b327088d4b148cb95b895
Klymov Sergii <klymovsergii05@gmail.com>
2018-08-16 15:25:33 +0300

    updatin main page,add css

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.header')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 garage">
                 <a href="{{url('garage')}}">Garage</a>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 user">
                 <a href="{{url('user')}}">User</a>
             </div>
         </div>
